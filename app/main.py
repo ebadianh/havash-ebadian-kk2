c26a3a39a265808468b685cf4e7b656805abe481
Havash <ebadian1992@gmail.com>
2026-05-26 22:11:00 +0200

    Create CSV upload endpoint to allow football datasets to be analyzed with pandas

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import pandas as pd
 
 app = FastAPI()
 
 @app.get("/health")
 def health():
   return {"status": "OK"}
+
+@app.post("/data/upload")
+async def upload_data(file: UploadFile = File(...)):
+  df = pd.read_csv(file.file)
+
+  return {
+    "rows": len(df),
+    "columns": list(df.columns)
+  }
 
